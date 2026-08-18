52526d87890d5f407bcb41904c6a9bbd5034e2f9
akashmondkar7 <akashmondkar03@gmail.com>
2026-08-18 20:43:54 +0530

    updated project

--- backend/services/auth/config/firebase.js
+++
@@ -1,7 +0,0 @@
-import { initializeApp, cert } from "firebase-admin/app";
-
-import serviceAccount from "../serviceAccount.json" with { type: "json" };
-
-export const app = initializeApp({
-  credential: cert(serviceAccount),
-});
