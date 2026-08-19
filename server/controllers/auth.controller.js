5116c4b82b9221613ad7a8bc98478d67be2dd7d2
akashmondkar7 <akashmondkar03@gmail.com>
2026-08-19 22:00:10 +0530

    auth issue

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -15,8 +15,8 @@ export const googleAuth = async (req,res) => {
         let token = await getToken(user._id)
         res.cookie("token" , token , {
             httpOnly:true,
-            secure:true,
-            samesite:"none",
+            secure:false,
+            sameSite:"lax",
             maxAge:7 * 24 * 60 * 60 * 1000
 
         })
@@ -29,7 +29,11 @@ export const googleAuth = async (req,res) => {
 
 export const logOut = async (req,res) => {
     try {
-        await res.clearCookie("token")
+        await res.clearCookie("token", {
+            httpOnly: true,
+            secure: false,
+            sameSite: "lax"
+        })
          return res.status(200).json({message:"LogOut Successfully"})
     } catch (error) {
         return res.status(500).json({message:`Logout Error  ${error}`})
