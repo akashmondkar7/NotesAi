66a13519485da266b98306245c7a19dcdb78e1d0
akashmondkar7 <akashmondkar03@gmail.com>
2026-08-21 22:51:33 +0530

    api

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -4,11 +4,11 @@ import {getAuth, GoogleAuthProvider} from "firebase/auth"
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "authexamnotes.firebaseapp.com",
-  projectId: "authexamnotes",
-  storageBucket: "authexamnotes.firebasestorage.app",
-  messagingSenderId: "198091553006",
-  appId: "1:198091553006:web:cd719d4ee1ebf391139bff"
+  authDomain: "examnotesai-3fc66.firebaseapp.com",
+  projectId: "examnotesai-3fc66",
+  storageBucket: "examnotesai-3fc66.firebasestorage.app",
+  messagingSenderId: "549868605364",
+  appId: "1:549868605364:web:6b8782228aa4a1c8966ce1"
 };
 
 
