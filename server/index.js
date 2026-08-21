6404976fc768de3d65fa36db46ac4c863187e587
akashmondkar7 <akashmondkar03@gmail.com>
2026-08-22 01:01:31 +0530

    save

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ dotenv.config()
 const app = express()
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://notes-ai-mauve-tau.vercel.app",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
